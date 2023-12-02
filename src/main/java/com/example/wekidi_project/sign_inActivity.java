0c8a1fe942eeba08d0c8f55170c9cb910d6fcd39
Muhammad Fadli Irjiyansyah <86676501+Fadliirjiyansy@users.noreply.github.com>
2023-12-02 10:04:51 +0700

    Add files via upload

---
+++ src/main/java/com/example/wekidi_project/sign_inActivity.java
@@ -0,0 +1,14 @@
+package com.example.wekidi_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_inActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
